ee7c842847c4aa4db5b9f7a730cfdf2fafa6473b
Shoaib <shoaibchemistrysust@gmail.com>
2024-12-01 23:20:20 +0600

    paritially add money event handler added

--- js/home.js
+++ js/home.js
@@ -1,1 +1,13 @@
-console.log('home js file created');
+//Add money
+//step-1: add an event handler to the  money button inside the form
+document.getElementById('btn-add-money')
+.addEventListener('click', function(event) {
+    //prevent page reload or form behaviour
+    event.preventDefault();
+    //step-2: get money to be added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    //step-3: get pin number
+    const pinInput = document.getElementById('input-pin').value;
+    console.log(addMoneyInput, pinInput);
+
+})
